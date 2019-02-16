c4b89b5e2e28d798713e65a553041277706e8bc2
jfernando <jfhernan@ualberta.ca>
2019-02-15 19:40:40 -0700

    added shell scripts for running experiments

---
+++ Shell_Scripts/ExperienceReplay/BufferSize20000_Freq1.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+echo Enter number of runs:
+read number_of_runs
+
+export PYTHONPATH=.
+for (( i=1; i <= $number_of_runs; i++ ))
+do
+    echo "Run $i..."
+    python3 ./ExperienceReplay_Experiment.py -buffer_size 20000 -tnet_update_freq 1
+done
