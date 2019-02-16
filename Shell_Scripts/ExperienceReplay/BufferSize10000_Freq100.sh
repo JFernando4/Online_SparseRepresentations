4f05ad8d3e87714dadc377d9755ce25d1a522f02
jfernando <jfhernan@ualberta.ca>
2019-02-16 13:59:49 -0700

    added shell scripts for running experiments

--- Shell_Scripts/ExperienceReplay/BufferSize10000_Freq100.sh
+++ Shell_Scripts/ExperienceReplay/BufferSize10000_Freq100.sh
@@ -7,5 +7,5 @@ export PYTHONPATH=.
 for (( i=1; i <= $number_of_runs; i++ ))
 do
     echo "Run $i..."
-    python3 ./ExperienceReplay_Experiment.py -buffer_size 10000 -tnet_update_freq 100
+    python3 ./ExperienceReplay_Experiment.py -buffer_size 10000 -tnet_update_freq 100 -verbose
 done
