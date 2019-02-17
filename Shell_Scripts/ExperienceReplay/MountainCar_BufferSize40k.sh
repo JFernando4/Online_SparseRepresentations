aef450376ca6d048c2836a03d23bc9c56b601aa6
jfernando <jfhernan@ualberta.ca>
2019-02-17 12:50:47 -0700

    shell script for mountain car with buffer size of 40k

---
+++ Shell_Scripts/ExperienceReplay/MountainCar_BufferSize40k.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+number_of_runs=$1
+
+export PYTHONPATH=.
+for FREQ in 1 10 50 100 200 400
+do
+    echo "Target Network Update Frequency: $FREQ"
+    for ((i=1; i <= $number_of_runs; i++))
+    do
+        echo "Run $i..."
+        python3 ./ExperienceReplay_Experiment.py -buffer_size 40000 -tnet_update_freq $FREQ -verbose
+    done
+done
