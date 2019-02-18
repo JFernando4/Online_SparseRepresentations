074830dae8694a0b9fd056707cf92af36a97df64
jfernando <jfhernan@ualberta.ca>
2019-02-18 12:43:31 -0700

    changed the value of LR to be provided as an argument

--- Shell_Scripts/SigmoidWeightedUnits/MountainCar_SigmoidWeightedUnits_SingleLearningRate.sh
+++ Shell_Scripts/SigmoidWeightedUnits/MountainCar_SigmoidWeightedUnits_SingleLearningRate.sh
@@ -2,16 +2,14 @@
 
 number_of_runs=$1
 architecture=$2
+LR=$3
 
-export PYTHONPATH=.
-for LR in 0.01 0.004 0.001 0.00025 0.0000625
+
+echo "Learnng Rate: $LR"
+for ((i=1; i <= $number_of_runs; i++))
 do
-    echo "Learnng Rate: $LR"
-    for ((i=1; i <= $number_of_runs; i++))
-    do
-        echo "Run $i..."
-        python3 ./SigmoidWeightedUnits_Experiment.py -lr $LR -architecture $architecture -verbose
-    done
+    echo "Run $i..."
+    python3 ./SigmoidWeightedUnits_Experiment.py -lr $LR -architecture $architecture -verbose
 done
 
 # Parameter Sweep:
