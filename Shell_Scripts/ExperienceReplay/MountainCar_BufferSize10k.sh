86d93932660dbaeb4b64de03f877d2dc7e9feed8
jfernando <jfhernan@ualberta.ca>
2019-02-17 18:40:23 -0700

    removed target network frequency of 1 because it rarely works

--- Shell_Scripts/ExperienceReplay/MountainCar_BufferSize10k.sh
+++ Shell_Scripts/ExperienceReplay/MountainCar_BufferSize10k.sh
@@ -3,7 +3,7 @@
 number_of_runs=$1
 
 export PYTHONPATH=.
-for FREQ in 1 10 50 100 200 400
+for FREQ in 10 50 100 200 400
 do
     echo "Target Network Update Frequency: $FREQ"
     for ((i=1; i <= $number_of_runs; i++))
