801ab486a43c6be96349fd39a8e6c8f7e4600566
jfernando <jfhernan@ualberta.ca>
2019-02-22 17:22:13 -0700

    increased the time to 2:30 hours

--- Slurm_Scheduling_Files/mc_experience_replay.sh
+++ Slurm_Scheduling_Files/mc_experience_replay.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 #SBATCH --mail-user=jfhernan@ualberta.ca
 #SBATCH --mail-type=ALL
-#SBATCH --array=1-30%1
-#SBATCH --time=1:30:00
+#SBATCH --array=1-30
+#SBATCH --time=2:30:00
 #SBATCH --account=def-sutton
 #SBATCH --mem=1000M
 #SBATCH --job-name=mc_er
